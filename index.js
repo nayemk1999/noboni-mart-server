a95ecb8a2b0280d6ba270b2036e5fee371fc1898
Nayem <nayemk900@gmail.com>
2021-03-31 01:07:22 +0600

    add Products part done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors');
 const bodyParser = require('body-parser');
+require('dotenv').config()
 const MongoClient = require('mongodb').MongoClient;
 const {DB_USER, DB_PASS, DB_NAME} = process.env
 const port =process.env.PORT || 3005
@@ -12,8 +13,23 @@ app.use(bodyParser.json())
 const uri = `mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.hyqj7.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log('Connected Server to mongodb');
+  const productsCollection = client.db("noboniMart").collection("products");
+  
+  app.post('/addProducts', (req, res) => {
+      const newProduct = req.body
+      productsCollection.insertOne(newProduct)
+      .then(result => {
+          res.send(result.insertedCount > 0)
+      })
+  });
+
+  app.get('/products', (req, res) => {
+    productsCollection.find({})
+    .toArray((error, document) => {
+        res.send(document)
+    })
+  })
+  
 });
 
 
